230b3c120ba8f8470735d0d8dbb372f96698b4e4
alfredcalayag <rapadura05@gmail.com>
2016-09-12 03:42:58 -0700

    Add deployment files

--- server/app.js
+++ server/app.js
@@ -4,36 +4,23 @@
 
 'use strict';
 
-import express from 'express';
-import mongoose from 'mongoose';
-mongoose.Promise = require('bluebird');
-import config from './config/environment';
-import http from 'http';
+var express = require('express');
+var app = express();
 
-// Connect to MongoDB
-mongoose.connect(config.mongo.uri, config.mongo.options);
-mongoose.connection.on('error', function(err) {
-  console.error('MongoDB connection error: ' + err);
-  process.exit(-1);
+// set the port of our application
+// process.env.PORT lets the port be set by Heroku
+var port = process.env.PORT || 8080;
+
+// make express look in the public directory for assets (css/js/img)
+app.use(express.static(__dirname + '/app'));
+
+// set the home page route
+app.get('/', function(req, res) {
+
+    // make sure index is in the right directory. In this case /app/index.html
+    res.render('index');
 });
 
-// Populate databases with sample data
-if (config.seedDB) { require('./config/seed'); }
-
-// Setup server
-var app = express();
-var server = http.createServer(app);
-require('./config/express').default(app);
-require('./routes').default(app);
-
-// Start server
-function startServer() {
-  app.angularFullstack = server.listen(config.port, config.ip, function() {
-    console.log('Express server listening on %d, in %s mode', config.port, app.get('env'));
-  });
-}
-
-setImmediate(startServer);
-
-// Expose app
-exports = module.exports = app;
+app.listen(port, function() {
+    console.log('Our app is running on http://localhost:' + port);
+});
